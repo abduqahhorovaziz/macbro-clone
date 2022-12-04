ae3040d5ee7d5457136e2ede9b58591e2047ed3b
abduqahhorovaziz <aziz.abduqahhorov6111@gmail.com>
2022-12-04 21:13:46 +0500

    feat: responsive done

--- js/main.js
+++ js/main.js
@@ -1,38 +1,43 @@
-// create an array of objects with the id, trigger element (eg. button), and the content element
-const tabElements = [
-    {
-        id: 'profile',
-        triggerEl: document.querySelector('#profile-tab-example'),
-        targetEl: document.querySelector('#profile-example')
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".slideNextBtn",
+    prevEl: ".slidePrevBtn",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
     },
-    {
-        id: 'dashboard',
-        triggerEl: document.querySelector('#dashboard-tab-example'),
-        targetEl: document.querySelector('#dashboard-example')
+    375: {
+      slidesPerView: 1,
     },
-    {
-        id: 'settings',
-        triggerEl: document.querySelector('#settings-tab-example'),
-        targetEl: document.querySelector('#settings-example')
-    }
-];
+    425: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
 
-// options with default values
-const options = {
-    defaultTabId: 'settings',
-    activeClasses: 'text-blue-600 bg-black hover:text-blue-600 border-black',
-    inactiveClasses: 'text-gray-500 hover:text-gray-600 border-gray-100 hover:border-gray-300',
-    onShow: () => {
-        console.log('tab is shown');
-    }
+  },
+});
+
+const openMenu = document.querySelector("#burgerNav");
+const closeMenu = document.querySelector("#closeNav");
+const mobileMenu = document.querySelector("#mobileNav");
+
+openMenu.onclick = () => {
+  mobileMenu.classList.remove("hidden");
+  openMenu.classList.add("hidden");
+  closeMenu.classList.remove("hidden");
 };
 
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: '.slideNextBtn',
-        prevEl: '.slidePrevBtn',
-      },
-  });
+closeMenu.onclick = () => {
+  mobileMenu.classList.add("hidden");
+  openMenu.classList.remove("hidden");
+  closeMenu.classList.add("hidden");
+};
